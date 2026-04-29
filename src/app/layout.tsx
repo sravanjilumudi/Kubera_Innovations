c6a138f798a7e47c87f126b0aa51c6b16cbe3285
sravan <166795037+sravanjilumudi@users.noreply.github.com>
2026-04-29 07:11:57 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,8 +66,8 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
-}, {
+  children: React.ReactNode;
+}>) {
   return (
     <html
       lang="en"
@@ -79,5 +79,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
